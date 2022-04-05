461f1763f685df0a12910372c62c56af926c8e16
aditya9090400 <aditya9090400@gmail.com>
2022-04-05 12:52:07 +0530

    permission added to book details view and tests added

--- books/views.py
+++ books/views.py
@@ -1,15 +1,16 @@
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Book
 
 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
-    template_name = 'books/book_list.html'
-    context_object_name = 'book_list'
+    template_name = "books/book_list.html"
+    context_object_name = "book_list"
 
 
-class BookDetailView(LoginRequiredMixin, DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
-    template_name = 'books/book_detail.html'
-    context_object_name = 'book'
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
+    permission_required = 'books.special_status'
